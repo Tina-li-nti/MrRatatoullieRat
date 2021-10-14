e7e7caa126edb0bfc6567ec88bf91a46129f1783
Tina Li <tina.li@elev.ga.ntig.se>
2021-10-14 11:10:17 +0200

    tdrytfugyijop

--- MrRatatoullieRat/Program.cs
+++ MrRatatoullieRat/Program.cs
@@ -7,7 +7,38 @@ namespace MrRatatoullieRat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ratClass rat = new ratClass()
+            {
+
+            };
+            Console.WriteLine("Please assign Remmy the rat his skills");
+            Console.WriteLine("Available ponits: 250");
+            Console.WriteLine("soupMaker: ");
+            string soupMakerS;
+            soupMakerS = Console.ReadLine();
+            int soupResultat;
+            bool goodSoup = int.TryParse(soupMakerS, out soupResultat);
+            rat.soupMaker = soupResultat;
+            Console.WriteLine("beefSteaker: ");
+            string beefSteakerS;
+            beefSteakerS = Console.ReadLine();
+            int beefResultat;
+            bool goodBeef = int.TryParse(beefSteakerS, out beefResultat);
+            rat.beefSteaker = beefResultat;
+            Console.WriteLine("pastaBaker: ");
+            string pastaBakerS;
+            pastaBakerS = Console.ReadLine();
+            int pastaResultat;
+            bool goodPasta = int.TryParse(pastaBakerS, out pastaResultat);
+            rat.pastaBaker = pastaResultat;
+            Console.WriteLine("girlfriendTaker: ");
+            Console.WriteLine(rat.soupMaker + rat.beefSteaker);
+            Console.ReadLine();
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ MrRatatoullieRat/ratClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MrRatatoullieRat
+{
+    public class ratClass
+    {
+        public string name = "Remmy";
+        public int soupMaker { get; set; }
+        public int beefSteaker  { get; set; }
+        public int pastaBaker  { get; set; }
+        public int girlfriendTaker  { get; set; }
+    }
+}
